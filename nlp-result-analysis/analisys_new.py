c6ecc076b7595d83875718ac7cebc1003b2f4bc7
Anat Samohi <anatsamohi@gmail.com>
2021-05-06 12:17:49 +0300

    clea files and add aalysis files

---
+++ nlp-result-analysis/analisys_new.py
@@ -0,0 +1,106 @@
+
+from scipy import stats
+import random
+import matplotlib.pyplot as plt
+
+def Aalysis(file_path, color, subplot_arg):
+    file=open(file_path)
+
+    languages = {
+      'EN':   {'lan': 0, 'di': 0, 'sub-fa': 'Ge', 'fa': 0},
+      'DE':    {'lan': 1, 'di': 0, 'sub-fa': 'Ge', 'fa': 0},
+      'NL':     {'lan': 2, 'di': 0, 'sub-fa': 'Ge', 'fa': 0},
+      'ES':   {'lan': 3, 'di': 0, 'sub-fa': 'Ro', 'fa': 0},
+      'PT': {'lan': 3, 'di': 1, 'sub-fa': 'Ro', 'fa': 0},
+      'FR':    {'lan': 4, 'di': 0, 'sub-fa': 'Ro', 'fa': 0},
+      'PL':    {'lan': 5, 'di': 0, 'sub-fa': 'Sl', 'fa': 0},
+      'HU': {'lan': 6, 'di': 0, 'sub-fa': 'Ug', 'fa': 1},
+      'FI':   {'lan': 7, 'di': 0, 'sub-fa': 'Ba', 'fa': 1}
+    }
+
+    languages_distances_by_accuracy = {
+      'EN':   {},
+      'DE':    {},
+      'NL':     {},
+      'ES':   {},
+      'PT': {},
+      'FR':    {},
+      'PL':    {},
+      'HU': {},
+      'FI':   {}
+    }
+
+    groups = {0:[], 1:[], 2:[], 3:[], 4:[]}
+    couples = []
+
+    for line in file:
+        line = line.replace('/', ' ').replace('\n', ' ')
+        location = [word for word in line.split()]
+
+        res = file.readline()
+        res = [word for word in res.split()]
+
+        print(res[-1])
+        accuracy = float(res[-1])
+        trained_lan  = location[-2]
+        second_lan = location[-1]
+
+        languages_distances_by_accuracy[trained_lan][second_lan] = (accuracy)
+        if languages[trained_lan]['lan'] == languages[second_lan]['lan']:
+            if languages[trained_lan]['di'] == languages[second_lan]['di']:
+                groups[0].append(accuracy)
+                couples.append((0, accuracy))
+            else:
+                groups[1].append(accuracy)
+                couples.append((1, accuracy))
+        else:
+            if languages[trained_lan]['sub-fa'] == languages[second_lan]['sub-fa']:
+                groups[2].append(accuracy)
+                couples.append((2, accuracy))
+            else:
+                if languages[trained_lan]['fa'] == languages[second_lan]['fa']:
+                    groups[3].append(accuracy)
+                    couples.append((3, accuracy))
+                else:
+                    groups[4].append(accuracy)
+                    couples.append((4, accuracy))
+
+    file.close()
+
+    print(languages_distances_by_accuracy)
+    print("The average f1:")
+    print(" %.4f if train on the same language" % (sum(groups[0]) / len(groups[0])))
+    #print(" %.4f if train on a different dialect" % (sum(groups[1]) / len(groups[1])))
+    print(" %.4f if train on a language from the same sub-family" % (sum(groups[2]) / len(groups[2])))
+    print(" %.4f if train on a language from the same family" % (sum(groups[3]) / len(groups[3])))
+    print(" %.4f if train on a language from the different family\n" % (sum(groups[4]) / len(groups[4])))
+
+    random.shuffle(couples)
+    x=[element[0] for element in couples]
+    y=[element[1] for element in couples]
+
+    rho, pval = stats.spearmanr(couples)
+    print('Spearman: rho=' + str(rho) +' p_value=' +str(pval))
+
+
+    rho, pval = stats.pearsonr(x,y)
+    print('Pearson: rho=' + str(rho) +' p_value=' +str(pval))
+    subplot_arg.scatter(x,y, marker= "*", color=color)
+    subplot_arg.axis([-0.5, 4.5, 0.4, 1])
+
+    subplot_arg.set_xlabel('')
+    subplot_arg.set_ylabel('cross train f1 score')
+    subplot_arg.set_xticks([-1, 0, 1, 2, 3, 4, 5])
+    subplot_arg.set_xticklabels(['','same language', 'dialects', 'same subgroup', 'same family', 'different families', ' '])
+
+
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+fig.set_size_inches(15, 10)
+fig.suptitle('Performance')
+ax1.set_title('NO FINE-TUNING')
+
+print('-----NO FINE-TUNING-----')
+Aalysis("./print_pre_results/result_prerun.txt", 'blue', ax1)
+
+plt.show()
--- transformers/examples/token-classification/print_pre_an.sh
+++ transformers/examples/token-classification/print_pre_an.sh
@@ -1,59 +1,52 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+#!/bin/bash
 
-\f0\fs24 \cf0 #!/bin/bash\
-\
-export MAIN_DIR=/home/tapuz/language_distancing/transformers/examples/token-classification\
-\
-echo START_ PRINT\
-languages=(\
-  english\
-  german\
-  dutch\
-  spanish\
-  portugese\
-  french\
-  polish\
-  hungarian\
-  finnish\
-)\
-\
-cd $MAIN_DIR #0\
-#echo "0 Moved to dir: `pwd`"\
-\
-for language_from in "$\{languages[@]\}"; do\
-\
-  if [ -d $language_from ]; then\
-    cd $language_from #1\
-    #echo "Moved to dir: `pwd`"\
-\
-    for language_to in "$\{languages[@]\}"; do\
-      if [ -d $language_to ]; then\
-        cd $language_to #2\
-        echo "Moved to dir: `pwd`"\
-        prerun_dir="postagger-$\{language_from\}-$\{language_to\}-model-prerun"\
-\
-        cd $prerun_dir #3\
-        #echo "Moved to dir: `pwd`"\
-              #echo "prerun $\{language_to\}"\
-        cat test_results.txt | grep eval_f1\
-        cd ../ #3\
-\
-        afterrun_dir="postagger-$\{language_from\}-$\{language_to\}-model-afterrun"\
-        cd $afterrun_dir #3\
-        #echo "Moved to dir: `pwd`"\
-              #echo "afterrun $\{language_to\}"\
-        #cat test_results.txt | grep eval_f1\
-        cd ../ #3\
-        cd ../ #2\
-\
-      fi\
-    done\
-\
-    cd ../ #1\
-  fi\
-done}
+export MAIN_DIR=/home/cs08/DanielAnat/GitProjects/transformers/examples/legacy/token-classification
+
+echo START_ PRINT
+languages=(
+  EN
+  DE
+  NL
+  ES
+  PT
+  FR
+  PL
+  HU
+  FI
+)
+
+cd $MAIN_DIR #0
+#echo "0 Moved to dir: `pwd`"
+
+for language_from in "${languages[@]}"; do
+
+  if [ -d $language_from ]; then
+    cd $language_from #1
+    #echo "Moved to dir: `pwd`"
+
+    for language_to in "${languages[@]}"; do
+      if [ -d $language_to ]; then
+        cd $language_to #2
+        echo "Moved to dir: `pwd`"
+        prerun_dir="postagger-${language_from}-${language_to}-model-prerun"
+
+        cd $prerun_dir #3
+        #echo "Moved to dir: `pwd`"
+              #echo "prerun ${language_to}"
+        cat test_results.txt | grep eval_f1
+        cd ../ #3
+
+        #afterrun_dir="postagger-${language_from}-${language_to}-model-afterrun"
+        #cd $afterrun_dir #3
+        #echo "Moved to dir: `pwd`"
+              #echo "afterrun ${language_to}"
+        #cat test_results.txt | grep eval_f1
+        #cd ../ #3
+        cd ../ #2
+
+      fi
+    done
+
+    cd ../ #1
+  fi
+done
